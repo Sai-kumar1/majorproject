6e39548bbdd652d23d4f1ca30d745d3c942b9a7b
Sai Kumar <saikumar.nitap@gmail.com>
2022-02-25 22:07:52 +0530

    Code to be added for rules

---
+++ static/js/threshold.js
@@ -0,0 +1,10 @@
+exports.threshold= (count) =>{
+    if(count>=2){
+        //need to include timeperiod
+        //under 2 minutes if the threshld count is 2 then logout device, greater than 4 ban device
+        //count ==2 ->logout device
+        //count ==4 -> bandevice
+        console.log("you have reached the limit")
+    }
+
+}
