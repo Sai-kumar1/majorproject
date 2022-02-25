9186cd1de02a4b51a809d44ca71edaac1ddcfcd8
Sai Kumar <saikumar.nitap@gmail.com>
2022-02-25 22:05:58 +0530

    Handler for detect and file finder

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { countReset } = require('console');
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -23,7 +24,10 @@ app.get("/path.js",  (req, res) => {
     res.sendFile(path.join(__dirname +"/static"+"/js"+"/path.js"));
 });
 
-
+app.get("/detection.js",  (req, res) => {
+    res.set("Content-Type", "text/javascript; charset=utf-8");
+    res.sendFile(path.join(__dirname +"/static"+"/js"+"/detection.js"));
+});
 
 app.post("/createTripWire", async (req, res) => {
     
@@ -43,7 +47,7 @@ app.post("/login",async (req, res) => {
     let body = "";
     req.on('data',function(data){
         body+=data
-    })
+    });
     req.on('end',async function(){
         // console.log(body);
         requestBody = JSON.parse(body);
@@ -56,12 +60,65 @@ app.post("/login",async (req, res) => {
             await mongoOperations.updateSession(requestBody);
         }
         
-    })
+    });
     
     res.send(body);
     res.status(200);
 
     res.end();
-})
+});
+
+app.post("/detectTripWire", async (req, res) => {
+    
+    let body = "";
+    let count= 0;
+    req.on('data',function(data){
+        body+=data
+    })
+    req.on('end',async function(){
+        console.log(body);
+        requestBody = JSON.parse(body);
+        items=await mongoOperations.findItem({"session":requestBody.session});
+        requestBody["paths"].forEach(element => {
+           if( items.tripwire[element.location].includes(element.path)){
+                count+=1
+           } 
+
+             //need to handle the threshold here     
+        });
+        console.log(count);
+
+        // thresholdOps.threshold(count);
+
+        
+    })    
+    res.status(200);
+    res.end();
+});
+
+app.post("/sendfile",async (req,res)=>{
+    let body = "";
+    let filename = "";
+    req.on('data',function(data){
+        body+=data
+    })
+    req.on('end',async function(){
+        console.log(body);
+        requestBody = JSON.parse(body);
+        items=await mongoOperations.findItem({"session":requestBody.session});
+        
+        if (Object.keys(items[0]["tripwire"]).length>1){
+            filename = "detection.js"
+        }else{
+            filename = "path.js"
+
+        }
+        // thresholdOps.threshold(count); 
+        res.status(200).json(JSON.stringify({"filename":filename}));  
+    });  
+    
+    
+    // res.end();
+});
 
 app.listen(8000,"127.0.0.5",()=>{console.log(`listenig on port http://127.0.0.5:8000`)});
