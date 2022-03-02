1786c7c32d99e8b90a396a50d460eceb7c89f3e6
sahiti <sahitivss@gmail.com>
2022-03-02 16:36:07 +0530

    new version

---
+++ mj.py
@@ -0,0 +1,126 @@
+import email
+import json
+import urllib
+from urllib import request
+from mitmproxy import ctx,http
+import requests
+import re
+
+###
+# 1. check the login attributes in the request to confirm the user credability
+# 2. if user successfully logged in then send script file to the client in <head> tag when ever a response consists of html 
+###
+
+
+
+class login:
+    
+    username=""
+    email=""
+    filename=""
+    def load(self,loader):
+        ctx.options.http2 = False
+
+    def request(self,flow:http.HTTPFlow):
+        get_list=["/login","/signup","/"]
+        if flow.request.method == "POST" and flow.request.path in get_list:
+            request_text=flow.request.text
+            qs_dict=urllib.parse.parse_qs(request_text)
+            self.username=qs_dict["username"][0]
+            self.email=qs_dict["email"][0]
+
+
+    def response(self,flow:http.HTTPFlow):
+        restricted_list=["/login","/signup","/"]
+        cookie = flow.response.headers.get("Set-Cookie")
+        location = re.search("sessionid",str(cookie))
+        id = ""
+        if location:
+            for i in range(location.end()+1 , len(cookie)):
+                if cookie[i]==";":
+                    break
+                id+=cookie[i]
+        print(cookie)
+        print("\n",id)
+        if id!="" and flow.request.path in restricted_list:
+            requests.post("http://127.0.0.5:8000/login",json ={"username":self.username,"session":id,"email":self.email})
+            if flow.request.path not in restricted_list:
+                res = requests.post("http://127.0.0.5:8000/sendfile",json = {"session":id})
+                data = json.loads(json.loads(res.text))
+                self.filename = data["filename"]
+        
+        if flow.request.headers.get('Host')=="foraproject.pythonanywhere.com" and flow.response.headers.get('Content-Type') == 'text/html; charset=utf-8' and flow.request.path not in restricted_list :
+            if self.filename=="":
+                self.filename = "detection.js"
+            replaceString = '<script src = "https://code.jquery.com/jquery-3.4.1.min.js"></script><script src="http://127.0.0.5:8000/{file}"></script></head>'.format(file="path.js")
+            flow.response.text = flow.response.text.replace('</head>',replaceString)
+
+###
+# 0. check if user has session already.
+# 1. check if user has created the tripwires from db.
+# 2. if not, activate the tripwire creation in the js.
+# 3. if user creates the tripwire send them to the db
+
+###
+class createTripwire:
+    
+    # if user is not in database, add user to database and create tripwire
+    def load(self,loader):
+        ctx.options.http2 = False
+
+    def request(self,flow:http.HTTPFlow):
+        if flow.request.method == "POST" and flow.request.path == "/createTripWire":
+
+            cookie = flow.request.headers.get("Cookie")
+            location = re.search("sessionid",str(cookie))
+            id = ""
+            if location:
+                for i in range(location.end()+1 , len(cookie)):
+                    if cookie[i]==";":
+                        break
+                    id+=cookie[i]
+            body = json.loads(flow.request.text)
+            body["session"]=id
+            print(body)
+            # body["path"]=str(flow.request.path)
+            requests.post("http://127.0.0.5:8000/createTripWire",json = body)
+    
+    def response(self,flow:http.HTTPFlow):
+        if flow.response.status_code!=200 and flow.request.path=="/createTripWire":
+            flow.response.status_code = 200
+            flow.response.text = '{"status":"success"}'
+
+class detectTripwire:
+    
+    # if user is not in database, add user to database and create tripwire
+    def load(self,loader):
+        ctx.options.http2 = False
+
+    def request(self,flow:http.HTTPFlow):
+
+        if flow.request.method == "POST" and flow.request.path == "/detectTripWire":
+
+            cookie = flow.request.headers.get("Cookie")
+            location = re.search("sessionid",str(cookie))
+            id = ""
+            if location:
+                for i in range(location.end()+1 , len(cookie)):
+                    if cookie[i]==";":
+                        break
+                    id+=cookie[i]
+            body = json.loads(flow.request.text)
+            body["session"]=id
+            # print(body)
+            # body["path"]=str(flow.request.path)
+            res=requests.post("http://127.0.0.5:8000/detectTripWire",json = body)
+            print(res.text)
+            # res.text
+    
+    def response(self,flow:http.HTTPFlow):
+        if flow.response.status_code!=200 and flow.request.path=="/detectTripWire":
+            print("Success for detect")
+            flow.response.status_code = 200
+            flow.response.text = '{"status":"success"}' 
+            # response.post()       
+
+addons = [login(),createTripwire(),detectTripwire()]
---
+++ server.js
@@ -0,0 +1,137 @@
+const { countReset } = require('console');
+const express = require('express');
+const app = express();
+const path = require('path');
+const threshold=require('./static/js/threshold.js')
+const mongoOperations = require('./mongo.js');
+
+// print the request and response
+app.use((req, res, next) => {
+    console.log("Request: " + req.url);
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("Response: " + res.statusCode);
+    next();
+});
+
+
+app.use(express.static(__dirname+'/static'));
+
+app.get("/path.js",  (req, res) => {
+    res.set("Content-Type", "text/javascript; charset=utf-8");
+    res.sendFile(path.join(__dirname +"/static"+"/js"+"/path.js"));
+});
+
+app.get("/detection.js",  (req, res) => {
+    res.set("Content-Type", "text/javascript; charset=utf-8");
+    res.sendFile(path.join(__dirname +"/static"+"/js"+"/detection.js"));
+});
+
+app.post("/createTripWire", async (req, res) => {
+    
+    let body = "";
+    req.on('data',function(data){
+        body+=data
+    })
+    req.on('end',function(){
+        console.log(body);
+        mongoOperations.updateTripwire(body);
+    })    
+    res.status(200);
+    res.end();
+});
+
+app.post("/login",async (req, res) => {
+    let body = "";
+    req.on('data',function(data){
+        body+=data
+    });
+    req.on('end',async function(){
+        // console.log(body);
+        requestBody = JSON.parse(body);
+        items = await mongoOperations.findItem({"username":requestBody.username});
+        // console.log(items);
+        if(items.length == 0){
+            await mongoOperations.createItem(requestBody);
+        }
+        else{
+            await mongoOperations.updateSession(requestBody);
+        }
+        
+    });
+    
+    res.send(body);
+    res.status(200);
+
+    res.end();
+});
+
+app.post("/detectTripWire", async (req, res) => {
+    
+    let body = "";
+    let count= 0; 
+    let y ="";
+    req.on('data',function(data){
+        body+=data
+    })
+    req.on('end',async function(){
+        console.log(body);
+        requestBody = JSON.parse(body);
+        items=await mongoOperations.findItem({"session":requestBody.session});
+        if (items.length>0){
+            requestBody["paths"].forEach(element => {
+                console.log(element,items);
+               if( items[0].tripwire[element.location].length!=0 && (items[0].tripwire[element.location]).includes(element.path)){
+                    count+=1;
+                    
+               } 
+            //    console.log(items[0].tripwire[element.location])
+                 //need to handle the threshold here     
+            });
+            console.log(count);
+            let x=threshold.threshold(count);
+            y=threshold.handling(x,requestBody.session);
+
+        }
+        
+
+        // thresholdOps.threshold(count);
+
+        
+    }) 
+    if(y=="emptysession"){
+        res.send("sessionnull")
+    }   
+    res.status(200);
+    res.end();
+});
+
+app.post("/sendfile",async (req,res)=>{
+    let body = "";
+    let filename = "";
+    req.on('data',function(data){
+        body+=data
+    })
+    req.on('end',async function(){
+        console.log(body);
+        requestBody = JSON.parse(body);
+        items=await mongoOperations.findItem({"session":requestBody.session});
+        console.log("items is   ",items)
+        
+        if (items[0]["tripwire"]!=undefined && Object.keys(items[0]["tripwire"]).length>1){
+            filename = "detection.js"
+        }else{
+            filename = "path.js"
+
+        }
+        // thresholdOps.threshold(count); 
+        res.status(200).json(JSON.stringify({"filename":filename}));  
+    });  
+    
+    
+    // res.end();
+});
+
+app.listen(8000,"127.0.0.5",()=>{console.log(`listenig on port http://127.0.0.5:8000`)});
