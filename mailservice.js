1b7409bae552fff830378ab798b69a75cc780639
Sai Kumar <saikumar.nitap@gmail.com>
2022-03-03 10:39:09 +0530

    Code for sending email for users

--- mailservice.js
+++ mailservice.js
@@ -1,11 +1,37 @@
-const sendmail = require('sendmail')();
- 
-sendmail({
-    from: 'no-reply@foraproject.pythonanywhere.com',
-    to: 'saikumar.gudur123@gmail.com',
-    subject: 'test sendmail',
-    html: 'Mail of test sendmail ',
-  }, function(err, reply) {
-    console.log(err && err.stack);
-    console.dir(reply);
+// const sendmail = require('sendmail')();
+const nodemailer = require("nodemailer");
+// const fs = require("fs")
+
+// const data = fs.readFile("./mailTemplate.txt")
+
+
+var smtpTransport = nodemailer.createTransport({
+  service : "gmail",
+  host: "smtp.gmail.com",
+  auth:{
+    user:"411808@student.nitandhra.ac.in",
+    pass:"qvlbkmsnbrtsjdgm"
+  }
 });
+
+
+exports.sendMail = (userInfo) => {
+  let data = `hello ${userInfo.user},
+
+  We have found that some thing wrong is going on in your account.That may be an intruder.
+
+  So we are closing your account ,please log in again.
+
+  Thankyou,
+  https://foraproject.pythonanywhere.com`
+
+  smtpTransport.sendMail({
+      // from: 'no-reply@foraproject.pythonanywhere.com',
+      to: userInfo.email,
+      subject: "Account Closed",
+      text: data,
+    }, function(err, reply) {
+      console.log(err && err.stack);
+      console.dir(reply);
+  });
+}
