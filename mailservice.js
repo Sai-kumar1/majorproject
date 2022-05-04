408a4770f9944490822cd628a4e0079c2d5d8486
sai <saikumar.nitap@gmail.com>
2022-05-04 12:40:47 +0530

    Update mail template and connection to service

--- mailservice.js
+++ mailservice.js
@@ -14,18 +14,28 @@ var smtpTransport = nodemailer.createTransport({
 });
 
 
-exports.sendMail = (userInfo,messageData) => {
+exports.sendMail = async (userInfo,messageData) => {
   let data = mailTemplate.renderMailTemplate(userInfo);
   if (messageData!=""){
     data = messageData
   }
+  var mailinfo={
+    from:"411808@student.nitandhra.ac.in",
+    to:userInfo.email,
+    subject: "Alert info",
+    text: data,
+  }
   console.log("sending mail")
-  smtpTransport.sendMail({
-      to: userInfo.email,
-      subject: "Alert info",
-      text: data,
-    }, function(err, reply) {
-      console.log(err && err.stack);
-      console.dir(reply);
+  smtpTransport.sendMail(mailinfo, function(err, info) {
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("mail sent successfully");
+    }
+      // console.log(err && err.stack);
+      // console.dir(reply);
   });
 }
+
+// sendMail({user:"random",email:"rand6953@gmail.com"},"");
